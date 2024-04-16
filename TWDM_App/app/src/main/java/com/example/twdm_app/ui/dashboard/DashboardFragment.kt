9b666ff54c20e0deefe09e49d1d151d66984543a
Dan Costinas <danscostinas@gmail.com>
2024-04-16 23:00:21 +0300

    Change Netork scan dashboard to use viewmodel for state

--- TWDM_App/app/src/main/java/com/example/twdm_app/ui/dashboard/DashboardFragment.kt
+++ TWDM_App/app/src/main/java/com/example/twdm_app/ui/dashboard/DashboardFragment.kt
@@ -1,23 +1,17 @@
 package com.example.twdm_app.ui.dashboard
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.content.pm.PackageManager
 import android.net.wifi.WifiManager
-import android.os.Build
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -80,21 +74,28 @@ class DashboardFragment : Fragment() {
             textView.text = it
         }
 
+        dashboardViewModel.scanResults.observe(viewLifecycleOwner) {
+            val data = ArrayList<NetworkItemViewModel>()
+            for (i in it)
+            {
+                data.add(NetworkItemViewModel(i.SSID))
+            }
+            val adapter = NetworkItemAdapter(data)
+            binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
+            binding.recyclerView.adapter = adapter
+        }
+
         return root
     }
 
     @SuppressLint("MissingPermission")
     private fun scanSuccess(context: Context) {
         val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val dashboardViewModel =
+            ViewModelProvider(this)[DashboardViewModel::class.java]
         val results = wifiManager.scanResults
-        val data = ArrayList<NetworkItemViewModel>()
-        for (i in results)
-        {
-            data.add(NetworkItemViewModel(i.SSID))
-        }
-        val adapter = NetworkItemAdapter(data)
-        binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
-        binding.recyclerView.adapter = adapter
+        dashboardViewModel.onScanUpdate(results)
+
         Log.i("WIFI", results.toString())
     }
 
--- TWDM_App/app/src/main/java/com/example/twdm_app/ui/dashboard/DashboardViewModel.kt
+++ TWDM_App/app/src/main/java/com/example/twdm_app/ui/dashboard/DashboardViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.twdm_app.ui.dashboard
 
+import android.net.wifi.ScanResult
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,4 +11,17 @@ class DashboardViewModel : ViewModel() {
         value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
+
+    private val _scanResults = MutableLiveData<List<ScanResult>>().apply {
+        value = ArrayList()
+    }
+    val scanResults: LiveData<List<ScanResult>> = _scanResults
+
+    fun onScanUpdate(results: MutableList<ScanResult>)
+    {
+        _scanResults.value = results
+    }
+
+
+
 }
