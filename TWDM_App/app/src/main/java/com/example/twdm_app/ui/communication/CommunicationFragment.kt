5713939e5be838cdb90b0fe51c63ba0a481f5468
Dan Costinas <danscostinas@gmail.com>
2024-04-22 17:45:18 +0300

    More rename

--- TWDM_App/app/src/main/java/com/example/twdm_app/ui/communication/CommunicationFragment.kt
+++ TWDM_App/app/src/main/java/com/example/twdm_app/ui/communication/CommunicationFragment.kt
@@ -1,4 +1,4 @@
-package com.example.twdm_app.ui.notifications
+package com.example.twdm_app.ui.communication
 
 import android.os.Bundle
 import android.util.Log
@@ -8,11 +8,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.twdm_app.databinding.FragmentNotificationsBinding
+import com.example.twdm_app.databinding.FragmentCommunicationBinding
 
-class NotificationsFragment : Fragment() {
+class CommunicationFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentCommunicationBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,14 +23,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this)[NotificationsViewModel::class.java]
+        val communicationViewModel =
+            ViewModelProvider(this)[CommunicationViewModel::class.java]
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentCommunicationBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        communicationViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
 
--- TWDM_App/app/src/main/java/com/example/twdm_app/ui/communication/CommunicationViewModel.kt
+++ TWDM_App/app/src/main/java/com/example/twdm_app/ui/communication/CommunicationViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.twdm_app.ui.notifications
+package com.example.twdm_app.ui.communication
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class CommunicationViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
